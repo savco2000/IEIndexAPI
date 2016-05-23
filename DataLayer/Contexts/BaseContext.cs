bf25bb71b736ba097f213761d715c88103558042
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-05-23 02:09:00 -0400

    Adding IEIndexContext

---
+++ DataLayer/Contexts/BaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace DataLayer.Contexts
+{
+    public class BaseContext<TContext> : DbContext where TContext : DbContext
+    {
+        static BaseContext()
+        {
+            //Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer<TContext>(null);
+        }
+
+        protected BaseContext() : base("name=DefaultConnection") { }
+    }
+}
---
+++ DataLayer/Contexts/IEIndexContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using DataLayer.DomainModels;
+using DataLayer.DomainModels.EntityConfigurations;
+
+namespace DataLayer.Contexts
+{
+    public class IEIndexContext : BaseContext<IEIndexContext>
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ArticleConfiguration());
+            modelBuilder.Configurations.Add(new AuthorConfiguration());
+            modelBuilder.Configurations.Add(new SubjectConfiguration());
+        }
+    }
+}
