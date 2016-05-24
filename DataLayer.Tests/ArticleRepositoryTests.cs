32e3044c77c03ab084c604339ead5f35e5f84923
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-05-24 03:21:20 -0400

    refactoring ArticleRepositoryTests

--- DataLayer.Tests/ArticleRepositoryTests.cs
+++ DataLayer.Tests/ArticleRepositoryTests.cs
@@ -13,27 +13,95 @@ namespace DataLayer.Tests
 
         public ArticleRepositoryTests()
         {
-            _articles = new List<Article>
+            var authors = new List<Author>
             {
-                new Article
-                {
+                new Author { Id = 1, FirstName = "Stuart", LastName = "Anderson" },
+                new Author { Id = 2,FirstName = "Elaina", LastName = "Loveland" },
+                new Author { Id = 3,FirstName = "Susan", LastName = "Ladika" }
+            };
+
+            var subjects = new List<Subject>
+            {
+                new Subject {Id = 1,Name = "Ethics" },
+                new Subject {Id = 2,Name= "Global Citizenship" },
+                new Subject {Id = 3,Name = "Higher Education" },
+                new Subject {Id = 4,Name = "Public Policy" }
+            };
+
+            _articles = new List<Article> {
+                new Article {
                     Id = 1,
                     Title = "All-in-One Comprehensive Immigration Reform",
                     Page = 4,
                     Issue = Issues.Fall,
                     PublicationYear = PublicationYears.Y2013,
                     IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf"
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf",
+                    Authors = new List<Author> { authors.Single(x => x.FirstName == "Stuart" && x.LastName == "Anderson") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy")
+                    }
                 },
-                new Article
-                {
+                new Article {
                     Id = 2,
                     Title = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa",
                     Page = 20,
                     Issue = Issues.Summer,
                     PublicationYear = PublicationYears.Y2013,
                     IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf"
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf",
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
+                },
+                new Article {
+                    Id = 3,
+                    Title = "A Champion for Development, Security, and Human Rights: An Interview with Kofi Annan",
+                    Page = 4,
+                    Issue = Issues.Spring,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_mayjun13_voices.pdf",
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Ethics"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
+                },
+                new Article {
+                    Id = 4,
+                    Title = "A Perspective on Communism's Collapse and European Higher Education",
+                    Page = 34,
+                    Issue = Issues.Spring,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy")
+                    }
+                },
+                new Article {
+                    Id = 5,
+                    Title = "Building a Literate World",
+                    Page = 14,
+                    Issue = Issues.Fall,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Susan" && x.LastName == "Ladika") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
                 }
             };
         }
@@ -47,10 +115,12 @@ namespace DataLayer.Tests
                 _articles.ForEach(article => fakeCtx.Articles.Add(article));
 
                 var repo = new ArticleRepository(uow);
-                const int expectedCount = 2;
-                var actualCount = repo.All.Count();
+                const int expectedCount = 5;
+                var actualCount1 = repo.All.Count();
+                var actualCount2 = repo.AllIncluding(x => x.Authors).Count();
 
-                Assert.Equal(expectedCount, actualCount);
+                Assert.Equal(expectedCount, actualCount1);
+                Assert.Equal(expectedCount, actualCount2);
             }
         }
 
@@ -79,9 +149,9 @@ namespace DataLayer.Tests
                 _articles.ForEach(article => fakeCtx.Articles.Add(article));
 
                 var repo = new ArticleRepository(uow);
-                const string titleOfArticleToBeDeleted = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa";
 
                 var articleToDelete = repo.Find(2);
+                var titleOfArticleToBeDeleted = articleToDelete.Title;
                 repo.Delete(articleToDelete);
 
                 const int expectedCount = 0;
