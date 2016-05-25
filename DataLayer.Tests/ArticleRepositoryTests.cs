45b7e391fd73a160d816b4b44c274cca37da503b
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-05-25 17:31:58 -0400

    more refactoring of ArticleRepositoryTests

--- DataLayer.Tests/ArticleRepositoryTests.cs
+++ DataLayer.Tests/ArticleRepositoryTests.cs
@@ -1,124 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using DataLayer.Contexts;
 using DataLayer.DomainModels;
-using DataLayer.FakesForTesting;
 using DataLayer.Repositories;
 using Moq;
 using Xunit;
 
 namespace DataLayer.Tests
 {
-    public class when_querying_for_articles
+    public class when_querying_for_articles : IClassFixture<ArticleRepositoryTestsFixture>
     {
         private readonly Mock<IIEIndexContext> _mockIEIndexContext;
-        private readonly IIEIndexContext _fakeCtx;
 
-        public when_querying_for_articles()
+        public when_querying_for_articles(ArticleRepositoryTestsFixture fixture)
         {
-            _mockIEIndexContext = new Mock<IIEIndexContext>();
-            _mockIEIndexContext.Setup(x => x.Articles).Returns(new ArticleFakeDbSet());
-
-            var authors = new List<Author>
-            {
-                new Author { Id = 1, FirstName = "Stuart", LastName = "Anderson" },
-                new Author { Id = 2,FirstName = "Elaina", LastName = "Loveland" },
-                new Author { Id = 3,FirstName = "Susan", LastName = "Ladika" }
-            };
-
-            var subjects = new List<Subject>
-            {
-                new Subject {Id = 1,Name = "Ethics" },
-                new Subject {Id = 2,Name= "Global Citizenship" },
-                new Subject {Id = 3,Name = "Higher Education" },
-                new Subject {Id = 4,Name = "Public Policy" }
-            };
-
-            var articles = new List<Article> {
-                new Article {
-                    Id = 1,
-                    Title = "All-in-One Comprehensive Immigration Reform",
-                    Page = 4,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf",
-                    Authors = new List<Author> { authors.Single(x => x.FirstName == "Stuart" && x.LastName == "Anderson") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 2,
-                    Title = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa",
-                    Page = 20,
-                    Issue = Issues.Summer,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 3,
-                    Title = "A Champion for Development, Security, and Human Rights: An Interview with Kofi Annan",
-                    Page = 4,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_mayjun13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Ethics"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 4,
-                    Title = "A Perspective on Communism's Collapse and European Higher Education",
-                    Page = 34,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 5,
-                    Title = "Building a Literate World",
-                    Page = 14,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Susan" && x.LastName == "Ladika") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                }
-            };
-
-            _fakeCtx = _mockIEIndexContext.Object;
-            articles.ForEach(article => _fakeCtx.Articles.Add(article));
+            _mockIEIndexContext = fixture.MockIEIndexContext;
         }
 
         [Fact]
         public void all_should_return_all_the_articles()
         {
-            using (var uow = new UnitOfWork<IEIndexContext>(_fakeCtx))
+            using (var uow = new UnitOfWork<IEIndexContext>(_mockIEIndexContext.Object))
             {
                 var sut = new ArticleRepository(uow);
 
@@ -134,7 +35,7 @@ namespace DataLayer.Tests
         [Fact]
         public void allincluding_should_return_all_the_articles()
         {
-            using (var uow = new UnitOfWork<IEIndexContext>(_fakeCtx))
+            using (var uow = new UnitOfWork<IEIndexContext>(_mockIEIndexContext.Object))
             {
                 var sut = new ArticleRepository(uow);
 
@@ -151,7 +52,7 @@ namespace DataLayer.Tests
         [Fact]
         public void find_should_return_the_correct_article()
         {
-            using (var uow = new UnitOfWork<IEIndexContext>(_fakeCtx))
+            using (var uow = new UnitOfWork<IEIndexContext>(_mockIEIndexContext.Object))
             {
                 var sut = new ArticleRepository(uow);
 
@@ -165,128 +66,25 @@ namespace DataLayer.Tests
         }
     }
 
-    public class when_creating_a_new_article
+    public class when_creating_a_new_article : IClassFixture<ArticleRepositoryTestsFixture>
     {
         private readonly Mock<IIEIndexContext> _mockIEIndexContext;
-        private readonly IIEIndexContext _fakeCtx;
+        private readonly Article _newArticle;
 
-        public when_creating_a_new_article()
+        public when_creating_a_new_article(ArticleRepositoryTestsFixture fixture)
         {
-            _mockIEIndexContext = new Mock<IIEIndexContext>();
-            _mockIEIndexContext.Setup(x => x.Articles).Returns(new ArticleFakeDbSet());
-
-            var authors = new List<Author>
-            {
-                new Author { Id = 1, FirstName = "Stuart", LastName = "Anderson" },
-                new Author { Id = 2,FirstName = "Elaina", LastName = "Loveland" },
-                new Author { Id = 3,FirstName = "Susan", LastName = "Ladika" }
-            };
-
-            var subjects = new List<Subject>
-            {
-                new Subject {Id = 1,Name = "Ethics" },
-                new Subject {Id = 2,Name= "Global Citizenship" },
-                new Subject {Id = 3,Name = "Higher Education" },
-                new Subject {Id = 4,Name = "Public Policy" }
-            };
-
-            var articles = new List<Article> {
-                new Article {
-                    Id = 1,
-                    Title = "All-in-One Comprehensive Immigration Reform",
-                    Page = 4,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf",
-                    Authors = new List<Author> { authors.Single(x => x.FirstName == "Stuart" && x.LastName == "Anderson") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 2,
-                    Title = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa",
-                    Page = 20,
-                    Issue = Issues.Summer,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 3,
-                    Title = "A Champion for Development, Security, and Human Rights: An Interview with Kofi Annan",
-                    Page = 4,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_mayjun13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Ethics"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 4,
-                    Title = "A Perspective on Communism's Collapse and European Higher Education",
-                    Page = 34,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 5,
-                    Title = "Building a Literate World",
-                    Page = 14,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Susan" && x.LastName == "Ladika") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                }
-            };
-
-            _fakeCtx = _mockIEIndexContext.Object;
-            articles.ForEach(article => _fakeCtx.Articles.Add(article));
+            _mockIEIndexContext = fixture.MockIEIndexContext;
+            _newArticle = fixture.NewArticle;
         }
 
         [Fact]
         public void insertorupdate_should_persist_new_article()
         {
-            using (var uow = new UnitOfWork<IEIndexContext>(_fakeCtx))
+            using (var uow = new UnitOfWork<IEIndexContext>(_mockIEIndexContext.Object))
             {
                 var sut = new ArticleRepository(uow);
 
-                sut.InsertOrUpdate(new Article
-                {
-                    Title = "All-in-One Comprehensive Immigration Reform",
-                    Page = 4,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf"
-                });
+                sut.InsertOrUpdate(_newArticle);
 
                 _mockIEIndexContext.VerifyAll();
 
@@ -296,129 +94,25 @@ namespace DataLayer.Tests
         }
     }
 
-    public class when_updating_an_existing_article
+    public class when_updating_an_existing_article : IClassFixture<ArticleRepositoryTestsFixture>
     {
         private readonly Mock<IIEIndexContext> _mockIEIndexContext;
-        private readonly IIEIndexContext _fakeCtx;
+        private readonly Article _existingArticle;
 
-        public when_updating_an_existing_article()
+        public when_updating_an_existing_article(ArticleRepositoryTestsFixture fixture)
         {
-            _mockIEIndexContext = new Mock<IIEIndexContext>();
-            _mockIEIndexContext.Setup(x => x.Articles).Returns(new ArticleFakeDbSet());
-
-            var authors = new List<Author>
-            {
-                new Author { Id = 1, FirstName = "Stuart", LastName = "Anderson" },
-                new Author { Id = 2,FirstName = "Elaina", LastName = "Loveland" },
-                new Author { Id = 3,FirstName = "Susan", LastName = "Ladika" }
-            };
-
-            var subjects = new List<Subject>
-            {
-                new Subject {Id = 1,Name = "Ethics" },
-                new Subject {Id = 2,Name= "Global Citizenship" },
-                new Subject {Id = 3,Name = "Higher Education" },
-                new Subject {Id = 4,Name = "Public Policy" }
-            };
-
-            var articles = new List<Article> {
-                new Article {
-                    Id = 1,
-                    Title = "All-in-One Comprehensive Immigration Reform",
-                    Page = 4,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf",
-                    Authors = new List<Author> { authors.Single(x => x.FirstName == "Stuart" && x.LastName == "Anderson") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 2,
-                    Title = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa",
-                    Page = 20,
-                    Issue = Issues.Summer,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 3,
-                    Title = "A Champion for Development, Security, and Human Rights: An Interview with Kofi Annan",
-                    Page = 4,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_mayjun13_voices.pdf",
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Ethics"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                },
-                new Article {
-                    Id = 4,
-                    Title = "A Perspective on Communism's Collapse and European Higher Education",
-                    Page = 34,
-                    Issue = Issues.Spring,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Public Policy")
-                    }
-                },
-                new Article {
-                    Id = 5,
-                    Title = "Building a Literate World",
-                    Page = 14,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Susan" && x.LastName == "Ladika") },
-                    Subjects = new List<Subject>
-                    {
-                        subjects.Single(x => x.Name == "Higher Education"),
-                        subjects.Single(x => x.Name == "Global Citizenship")
-                    }
-                }
-            };
-
-            _fakeCtx = _mockIEIndexContext.Object;
-            articles.ForEach(article => _fakeCtx.Articles.Add(article));
+            _mockIEIndexContext = fixture.MockIEIndexContext;
+            _existingArticle = fixture.ExistingArticle;
         }
 
         [Fact]
         public void insertorupdate_should_update_existing_article()
         {
-            using (var uow = new UnitOfWork<IEIndexContext>(_fakeCtx))
+            using (var uow = new UnitOfWork<IEIndexContext>(_mockIEIndexContext.Object))
             {
                 var sut = new ArticleRepository(uow);
 
-                sut.InsertOrUpdate(new Article
-                {
-                    Id = 1,
-                    Title = "All-in-One Comprehensive Immigration Reform",
-                    Page = 4,
-                    Issue = Issues.Fall,
-                    PublicationYear = PublicationYears.Y2013,
-                    IsSupplement = false,
-                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf"
-                });
+                sut.InsertOrUpdate(_existingArticle);
 
                 _mockIEIndexContext.VerifyAll();
 
---
+++ DataLayer.Tests/ArticleRepositoryTestsFixture.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataLayer.Contexts;
+using DataLayer.DomainModels;
+using DataLayer.FakesForTesting;
+using Moq;
+
+namespace DataLayer.Tests
+{
+    public class ArticleRepositoryTestsFixture
+    {
+        public Mock<IIEIndexContext> MockIEIndexContext { get; set; }
+        public Article NewArticle { get; set; }
+        public Article ExistingArticle { get; set; }
+
+        public ArticleRepositoryTestsFixture()
+        {
+            MockIEIndexContext = new Mock<IIEIndexContext>();
+            MockIEIndexContext.Setup(x => x.Articles).Returns(new ArticleFakeDbSet());
+
+            NewArticle = new Article
+            {
+                Title = "All-in-One Comprehensive Immigration Reform",
+                Page = 4,
+                Issue = Issues.Fall,
+                PublicationYear = PublicationYears.Y2013,
+                IsSupplement = false,
+                Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf"
+            };
+
+            ExistingArticle = new Article
+            {
+                Id = 1,
+                Title = "All-in-One Comprehensive Immigration Reform",
+                Page = 4,
+                Issue = Issues.Fall,
+                PublicationYear = PublicationYears.Y2013,
+                IsSupplement = false,
+                Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf"
+            };
+
+            var authors = new List<Author>
+            {
+                new Author { Id = 1, FirstName = "Stuart", LastName = "Anderson" },
+                new Author { Id = 2,FirstName = "Elaina", LastName = "Loveland" },
+                new Author { Id = 3,FirstName = "Susan", LastName = "Ladika" }
+            };
+
+            var subjects = new List<Subject>
+            {
+                new Subject {Id = 1,Name = "Ethics" },
+                new Subject {Id = 2,Name= "Global Citizenship" },
+                new Subject {Id = 3,Name = "Higher Education" },
+                new Subject {Id = 4,Name = "Public Policy" }
+            };
+
+            var articles = new List<Article> {
+                new Article {
+                    Id = 1,
+                    Title = "All-in-One Comprehensive Immigration Reform",
+                    Page = 4,
+                    Issue = Issues.Fall,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_frontlines.pdf",
+                    Authors = new List<Author> { authors.Single(x => x.FirstName == "Stuart" && x.LastName == "Anderson") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy")
+                    }
+                },
+                new Article {
+                    Id = 2,
+                    Title = "From Undocumented Immigrant to Brain Surgeon: An Interview with Alfredo Quiñones-Hinojosa",
+                    Page = 20,
+                    Issue = Issues.Summer,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_julaug13_voices.pdf",
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
+                },
+                new Article {
+                    Id = 3,
+                    Title = "A Champion for Development, Security, and Human Rights: An Interview with Kofi Annan",
+                    Page = 4,
+                    Issue = Issues.Spring,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Hyperlink = "http://www.nafsa.org/_/File/_/ie_mayjun13_voices.pdf",
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Ethics"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
+                },
+                new Article {
+                    Id = 4,
+                    Title = "A Perspective on Communism's Collapse and European Higher Education",
+                    Page = 34,
+                    Issue = Issues.Spring,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Elaina" && x.LastName == "Loveland") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Public Policy")
+                    }
+                },
+                new Article {
+                    Id = 5,
+                    Title = "Building a Literate World",
+                    Page = 14,
+                    Issue = Issues.Fall,
+                    PublicationYear = PublicationYears.Y2013,
+                    IsSupplement = false,
+                    Authors = new List<Author> {authors.Single(x => x.FirstName == "Susan" && x.LastName == "Ladika") },
+                    Subjects = new List<Subject>
+                    {
+                        subjects.Single(x => x.Name == "Higher Education"),
+                        subjects.Single(x => x.Name == "Global Citizenship")
+                    }
+                }
+            };
+
+            articles.ForEach(article => MockIEIndexContext.Object.Articles.Add(article));
+        }
+    }
+}
