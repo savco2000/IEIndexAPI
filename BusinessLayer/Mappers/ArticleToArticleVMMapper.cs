178d9656fd9f8e89e2120c07ca867def79431bc4
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-06-05 20:45:28 -0400

    adding mappers

---
+++ BusinessLayer/Mappers/ArticleToArticleVMMapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using BusinessLayer.ViewModels;
+using DataLayer.DomainModels;
+
+namespace BusinessLayer.Mappers
+{
+    public class ArticleToArticleVMMapper
+    {
+        public ArticleVM Map(Article source)
+        {
+            var target = new ArticleVM
+            {
+                Title = source.Title,
+                Page = source.Page,
+                Issue = source.Issue.GetEnumDescription(),
+                PublicationYear = source.PublicationYear.GetEnumDescription(),
+                IsSupplement = source.IsSupplement,
+                Hyperlink = source.Hyperlink,
+                Authors = source.Authors.Select(author => new AuthorVM
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    Suffix = author.Suffix.GetEnumDescription()
+                }).ToList(),
+                Subjects = source.Subjects.Select(subject => new SubjectVM { Name = subject.Name }).ToList()
+            };
+
+            return target;
+        }
+
+        public ArticleVM Map(Article source, params object[] options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/Mappers/AuthorToAuthorVMMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using BusinessLayer.ViewModels;
+using DataLayer.DomainModels;
+
+namespace BusinessLayer.Mappers
+{
+    public class AuthorToAuthorVMMapper
+    {
+        public AuthorVM Map(Author source)
+        {
+            var target = new AuthorVM
+            {
+                FirstName = source.FirstName,
+                LastName = source.LastName,
+                Suffix = source.Suffix.GetEnumDescription(),
+                Articles = source.Articles.Select(article => new ArticleVM
+                {
+                    Title = article.Title,
+                    Page = article.Page,
+                    Issue = article.Issue.GetEnumDescription(),
+                    PublicationYear = article.PublicationYear.GetEnumDescription(),
+                    IsSupplement = article.IsSupplement,
+                    Hyperlink = article.Hyperlink
+                }).ToList()
+            };
+
+            return target;
+        }
+
+        public AuthorVM Map(Author source, params object[] options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
