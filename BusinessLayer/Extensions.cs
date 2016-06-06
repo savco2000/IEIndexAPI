39071eace311a268bc62d5820f44a71d39a99f4c
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-06-05 20:30:14 -0400

    adding string extension

---
+++ BusinessLayer/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BusinessLayer
+{
+    public static class Extensions
+    {
+        public static string GetEnumDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var attributes = (DescriptionAttribute[]) fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+    }
+}
