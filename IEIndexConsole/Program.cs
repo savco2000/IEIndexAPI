7ed8e2709a95ddc290ff76c8a8fdff5313f9ed12
unknown <kadimas1@16L-KADIMA.CBBB.local>
2016-06-07 08:41:30 -0400

    refactoring IEIndexConsole

--- IEIndexConsole/Program.cs
+++ IEIndexConsole/Program.cs
@@ -13,44 +13,48 @@ namespace IEIndexConsole
     {
         static void Main(string[] args)
         {
+            const int pageSize = 10;
+            const int pageNumber = 1;
+
             var kernel = new StandardKernel();
 
             var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
             
             kernel.Bind<IMapper>().ToConstant(config.CreateMapper());
+            kernel.Bind<IEIndexContext>().To<IEIndexContext>().InTransientScope();
 
             var mapper = kernel.Get<IMapper>();
 
-            var context1 = new IEIndexContext();
+            var context1 = kernel.Get<IEIndexContext>();
             //context1.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
             using (var uow = new UnitOfWork<IEIndexContext>(context1))
             {
                 var articleService = new ArticleService(uow, mapper);
-                var articles = articleService.GetEntities(new ArticleSearchBindingModel(), 10, 1)
+                var articles = articleService.GetEntities(new ArticleSearchBindingModel(), pageSize, pageNumber)
                     .ToList();
-                var articlesWithChildren = articleService.GetEntitiesWithChildren(new ArticleSearchBindingModel(), 10, 1)
+                var articlesWithChildren = articleService.GetEntitiesWithChildren(new ArticleSearchBindingModel(), pageSize, pageNumber)
                     .ToList();
             }
 
-            var context2 = new IEIndexContext();
+            var context2 = kernel.Get<IEIndexContext>();
             //context2.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
             using (var uow = new UnitOfWork<IEIndexContext>(context2))
             {
                 var authorService = new AuthorService(uow, mapper);
-                var authors = authorService.GetEntities(new AuthorSearchBindingModel(), 10, 1)
+                var authors = authorService.GetEntities(new AuthorSearchBindingModel(), pageSize, 1)
                     .ToList();
-                var authorsWithChildren = authorService.GetEntitiesWithChildren(new AuthorSearchBindingModel(), 10, 1)
+                var authorsWithChildren = authorService.GetEntitiesWithChildren(new AuthorSearchBindingModel(), pageSize, pageNumber)
                     .ToList();
             }
 
-            var context3 = new IEIndexContext();
+            var context3 = kernel.Get<IEIndexContext>();
             //context3.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
             using (var uow = new UnitOfWork<IEIndexContext>(context3))
             {
                 var subjectService = new SubjectService(uow, mapper);
-                var subjects = subjectService.GetEntities(new SubjectSearchBindingModel(), 10, 1)
+                var subjects = subjectService.GetEntities(new SubjectSearchBindingModel(), pageSize, pageNumber)
                     .ToList();
-                var subjectsWithChildren = subjectService.GetEntitiesWithChildren(new SubjectSearchBindingModel(), 10, 1)
+                var subjectsWithChildren = subjectService.GetEntitiesWithChildren(new SubjectSearchBindingModel(), pageSize, pageNumber)
                     .ToList();
             }
         }
