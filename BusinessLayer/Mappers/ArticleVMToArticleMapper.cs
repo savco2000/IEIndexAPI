1ef3f3a7808a5bb8048a66e38ffbcee07fadd81b
Savannah O. Kadima <skadima@council.bbb.org>
2016-08-07 05:12:17 -0400

    Adding ArticleVMToArticleMapper.cs

---
+++ BusinessLayer/Mappers/ArticleVMToArticleMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.ViewModels;
+using DataLayer.DomainModels;
+
+namespace BusinessLayer.Mappers
+{
+    public class ArticleVMToArticleMapper : IMapToNew<ArticleVM, Article>
+    {
+        public Article Map(ArticleVM source)
+        {
+            var target = new Article
+            {
+                Title = source.Title,
+                Page = source.Page,
+                Issue = (Issues)Enum.Parse(typeof(Issues), source.Issue),
+                PublicationYear = (PublicationYears)Enum.Parse(typeof(PublicationYears), source.PublicationYear),
+                IsSupplement = source.IsSupplement,
+                Hyperlink = source.Hyperlink,
+                Authors = source.Authors.Select(author => new Author
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    Suffix = (Suffixes) Enum.Parse(typeof(Suffixes), author.Suffix)
+                }) as ICollection<Author>,
+                Subjects = source.Subjects.Select(subject => new Subject { Name = subject.Name }) as ICollection<Subject>
+            };
+
+            return target;
+        }
+
+        public Article Map(ArticleVM source, params object[] options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
