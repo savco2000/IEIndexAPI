f1e2fb02567ab66f54f2afbef7bc217981e72100
Savannah O. Kadima <skadima@council.bbb.org>
2016-08-13 04:56:48 -0400

    Deleting unused code

--- BusinessLayer/MappingProfile.cs
+++ BusinessLayer/MappingProfile.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using AutoMapper;
 using BusinessLayer.DTOs;
 using DataLayer.DomainModels;
@@ -15,24 +14,12 @@ namespace BusinessLayer
                 .ForMember(dest => dest.PublicationYear, opt => opt.MapFrom(src => src.PublicationYear.GetEnumDescription()))
                 .ForMember(dest => dest.Authors, opt => opt.MapFrom(src => src.Authors.Select(author => author.FullName)))
                 .ForMember(dest => dest.Subjects, opt => opt.MapFrom(src => src.Subjects.Select(subject => subject.Name)));
-
-            //CreateMap<ArticleDTO, Article>()
-            //    .ForMember(dest => dest.Issue, opt => opt.MapFrom(src => (Issues) Enum.Parse(typeof(Issues), src.Issue)))
-            //    .ForMember(dest => dest.PublicationYear, opt => opt.MapFrom(src => (PublicationYears) Enum.Parse(typeof(PublicationYears), src.PublicationYear)));
-
-            //CreateMap<Author, AuthorDTO>()
-            //    .ForMember(dest => dest.Suffix, opt => opt.MapFrom(src => src.Suffix.GetEnumDescription()))
-            //    .ForMember(dest => dest.Articles, opt => opt.MapFrom(src => src.Articles.Select(article => article.Title)));
+            
             CreateMap<Author, AuthorDTO>()
                 .ForMember(dest => dest.Articles, opt => opt.MapFrom(src => src.Articles.Select(article => article.Title)));
-
-            //CreateMap<AuthorDTO, Author>()
-            //    .ForMember(dest => dest.Suffix, opt => opt.MapFrom(src => (Suffixes) Enum.Parse(typeof(Suffixes), src.Suffix)));
-
+            
             CreateMap<Subject, SubjectDTO>()
                 .ForMember(dest => dest.Articles, opt => opt.MapFrom(src => src.Articles.Select(article => article.Title)));
-
-            //CreateMap<SubjectDTO, Subject>();
         }
     }
 }
