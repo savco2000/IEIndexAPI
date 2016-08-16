583ae3efd709395238ae4a73006c9f6f34db7aa2
Savannah O. Kadima <skadima@council.bbb.org>
2016-08-16 07:47:44 -0400

    Adding SubjectServiceTests.cs

--- BusinessLayer/Services/SubjectService.cs
+++ BusinessLayer/Services/SubjectService.cs
@@ -16,6 +16,11 @@ namespace BusinessLayer.Services
     {
         private readonly IMapper _mapper;
 
+        public SubjectService(Repository<Subject> repository, IMapper mapper, ILog log) : base(repository, mapper, log)
+        {
+            _mapper = mapper;
+        }
+
         public SubjectService(IUnitOfWork uow, IMapper mapper, ILog log) : base(uow, mapper, log)
         {
             _mapper = mapper;
